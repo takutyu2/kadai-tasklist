a8b43bc69c30fe9a7c6d025eef84f7c9b80f4045
taku <taku.tyu.2@gmail.com>
2017-05-01 04:36:27 +0000

    crud tasklist

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスク新規作成</h1>
+    
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+        
+        {!! Form::label('content', 'タスク内容：') !!}
+        {!! Form::text('content') !!}
+        
+        {!! Form::submit('追加') !!}
+        
+    {!! Form::close() !!}
+
+@endsection
