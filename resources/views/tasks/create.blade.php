f3dcb127317229f2e6deff6dead7f944c84d733e
taku <taku.tyu.2@gmail.com>
2017-05-02 12:08:10 +0000

    form correction

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,28 +5,22 @@
     <h1>タスク新規作成</h1>
     
     <div class="row">
-        <div class="col-xs-12">
-            <div class="col-sm-offset-2 col-sm-8">
-                <div class="col-md-offset-2 col-md-8">
-                    <div class="col-lg-offset-3 col-lg-6">
-                        {!! Form::model($task, ['route' => 'tasks.store']) !!}
-                            
-                            <div class="form-group">
-                                {!! Form::label('status', 'ステータス：') !!}
-                                {!! Form::text('status', null, ['class' => 'form-control']) !!}
-                            </div>
-                            
-                            <div class="form-group">
-                                {!! Form::label('content', 'タスク内容：') !!}
-                                {!! Form::text('content', null, ['class' => 'form-control']) !!}
-                            </div>
-                            
-                            {!! Form::submit('追加', ['class' => 'btn btn-primary']) !!}
-                            
-                        {!! Form::close() !!}
-                    </div>
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+                
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス：') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
-            </div>
+                
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク内容：') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('追加', ['class' => 'btn btn-primary']) !!}
+                
+            {!! Form::close() !!}
         </div>
     </div>
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,24 +5,22 @@
     <h1>id：{{ $task->id }}のタスク編集ページ</h1>
     
     <div class="row">
-        <div class="col-xs-12">
-            <div class="col-sm-offset-2 col-sm-8">
-                <div class="col-md-offset-2 col-md-8">
-                    <div class="col-lg-offset-3 col-lg-6">
-                        {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-                            
-                            {!! Form::label('status', 'ステータス') !!}
-                            {!! Form::text('status', null, ['class' => 'form-control']) !!}
-                            
-                            {!! Form::label('content', 'タスク内容：') !!}
-                            {!! Form::text('content', null, ['class' => 'form-control']) !!}
-                            
-                            {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
-                            
-                        {!! Form::close() !!}
-                    </div>
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+                
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
-            </div>
+                
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク内容：') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
+                
+            {!! Form::close() !!}
         </div>
     </div>
 @endsection
