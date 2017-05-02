fb8384cb882bfe668c39fec4400b73cd26a560bb
taku <taku.tyu.2@gmail.com>
2017-05-02 12:30:15 +0000

    data-target correction

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <!-- ハンバーガーメニュー -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="navbar-collapse-tasklist" aria-expanded="false">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-tasklist" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
