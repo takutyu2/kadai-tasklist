4136ef7a206f98f779e47efdb470253c378864cc
taku <taku.tyu.2@gmail.com>
2017-05-02 10:43:52 +0000

    add Procfile

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <!-- ハンバーガーメニュー -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarNav" aria-expanded="false">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -13,7 +13,7 @@
             </div>
             
             <!-- 通常メニュー -->
-            <div class="collapse navbar-collapse" id="#navbarNav">
+            <div class="collapse navbar-collapse" id="#bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     <li>{!! link_to_route('tasks.create', '新規メッセージの投稿') !!}</li>
                 </ul>
